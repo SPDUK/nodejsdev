b93d0b12470a8d324280c0408c0842fbfdbead27
Steve <spdevuk@gmail.com>
2018-05-06 11:36:35 +0100

    Hello World (in different ways too)

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,1 +1,27 @@
 console.log('Hello world');
+
+const helloWorld2 = () => {
+  console.log('Hello world 2');
+};
+
+function helloWorld3() {
+  console.log('Hello World 3');
+}
+
+const helloWorld4 = num => {
+  console.log('Hello world ' + num);
+};
+
+const helloWorld5 = str => {
+  const splitString = str
+    .split('')
+    .reverse()
+    .join('');
+  console.log(splitString);
+};
+helloWorld2();
+helloWorld3();
+helloWorld4(4);
+helloWorld5('5 dlroW olleH');
+
+// just saying hello world in a bunch of ways for fun
