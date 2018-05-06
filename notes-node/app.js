44a2a1ab18c8d7ae954c9a8edbedbdc11119bdb5
Steve <spdevuk@gmail.com>
2018-05-06 13:29:09 +0100

    function to add from notes in app.js

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,7 +2,11 @@ console.log('Starting app.');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
+var numbers = notes.add(1, 5);
+console.log(numbers);
 
-fs.appendFileSync('greetings.txt', `Hello ${user.username}`);
+// var user = os.userInfo();
+
+// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}`);
---
+++ notes-node/notes.js
@@ -0,0 +1,8 @@
+module.exports.addNote = () => {
+  console.log('addNote');
+  return 'New note';
+};
+
+module.exports.add = (a, b) => {
+  return a + b;
+};
