5b9a8d2d551cdb08b50e35bb356c110f4c8948fe
Steve <spdevuk@gmail.com>
2018-05-15 20:58:38 +0100

    added json info & helloworld 3 + 3.js

---
+++ hello-world/3.js
@@ -0,0 +1,27 @@
+function printHelp() {
+  console.log("3.js (c) SPDUK");
+  console.log("");
+  console.log("usage:");
+  console.log("--help              print this help");
+  console.log('--file={NAME}       read the file of {NAME} and output it');
+  console.log('');
+}
+
+var args = require("minimist")(process.argv.slice(2), {
+  string: "name"
+})
+
+if (args.help || !args.file) {
+  printHelp();
+  process.exit(1);
+}
+
+var hello = require('./helloworld3.js');
+
+hello.say(args.file)
+  .val(function (contents) {
+    console.log(contents.toString());
+  })
+  .or(function (err) {
+    console.error(err);
+  })
