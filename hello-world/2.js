43bb9790a73be6d7f006ca270fdac80fc0eeb2c7
Steve <spdevuk@gmail.com>
2018-05-15 15:00:03 +0100

    printing hello world from a .txt file outside of js

--- hello-world/2.js
+++ hello-world/2.js
@@ -2,17 +2,22 @@ function printHelp() {
   console.log("2.js (c) SPDUK");
   console.log("");
   console.log("usage:");
-  console.log("--help         print this help");
-  console.log("--file={NAME}       read the file of {NAME} and output it");
-  console.log("");
+  console.log("--help              print this help");
+  console.log('--file={NAME}       read the file of {NAME} and output it');
+  console.log('');
 }
 
-var args = require("minimist")(process.argv.slice(2), { string: "name"})
+var args = require("minimist")(process.argv.slice(2), {
+  string: "name"
+})
 
-if (args.help || !args.name) {
+if (args.help || !args.file) {
   printHelp();
   process.exit(1);
 }
 var name = args.name;
 
-console.log("Hello " + name);
+
+var hello = require('./helloworld.js');
+var contents = hello.say(args.file);
+console.log(contents.toString());
